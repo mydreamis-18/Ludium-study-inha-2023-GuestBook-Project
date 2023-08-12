4b91659b19f2556efae9399bc26437b072383a1f
mydreamis-18 <hitoryok@naver.com>
2023-08-12 10:53:32 +0900

    [Feat] 컨트랙트 작성, 빌드 및 배포까지는 에러 없음

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,15 +1,59 @@
-// ##### self : 'BorshDeseiralize'와 'BorshSerialize'가 작동하기 위해 필요합니다. 스마트계약 자기 자신을 참조합니다.
-// ##### BorshDeserialize : 스마트계약의 함수를 호출하면서 인자를 함께 전달할 때, 제이슨(JSON) 타입의 인자를 역직렬화합니다.
-// ##### BorshSerialize : 스마트계약에서 결과를 다시 보낼 때 제이슨으로 직렬화합니다.
-// ##### near_bidgen : 스마트계약이라는 것을 알려주는 어노테이션으로 각 컨트랙트마다 'near_bindgen' 어노테이션과 함께 선언된 스트럭트(struct)가 적어도 하나씩은 있어야 합니다.
-use near_sdk::borch::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bidgen, AccountId};
+// ##### rust-analyzer 확장 프로그램 설치하고 작업함
 
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct postedMessage {
+// ##### near_bindgen : 스마트계약이라는 것을 알려주는 어노테이션으로 각 컨트랙트마다 'near_bindgen' 어노테이션과 함께 선언된 스트럭트(struct)가 적어도 하나씩은 있어야 합니다.
+use near_sdk::{env, log, near_bindgen, AccountId};
+
+// ##### self : 'BorshDeseiralize'와 'BorshSerialize'가 작동하기 위해 필요합니다. 스마트계약 자기 자신을 참조합니다.
+// ##### BorshSerialize와 BorshDeserialize는 바이너리 형식으로 데이터를 변환하거나, 반대로 변환하는 기능을 제공합니다.
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+
+// ##### Serialize 트레잇은 serde 프레임워크에서 제공하는 트레잇으로, JSON과 같은 외부 데이터 형식으로 변환할 수 있도록 하는 기능을 제공합니다.
+use near_sdk::serde::Serialize;
+
+// ##### 벡터는 메모리 상에 서로 이웃하도록 모든 값을 집어넣는 단일 데이터 구조 안에 하나 이상의 값을 저장하도록 해줍니다. 벡터는 같은 타입의 값만을 저장할 수 있습니다.
+use near_sdk::collections::Vector;
+
+// ##### 사용할 구조체 정의
+// ##### Serialize 트레잇 구현
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct PostedMessage {
     message: String,
     sender: AccountId,
+}
+
+// ##### 사실은 초기 값을 넣지 않고 상태 값을 정의하고 싶었음
+// ##### 혹시 언젠가 상호 작용 할 지도 모르니 pub로 선언함
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct GuestBook {
+    posted_messages: Vector<PostedMessage>,
+}
+
+impl Default for GuestBook {
+    fn default() -> Self {
+        Self {
+            posted_messages: Vector::new(b"m"),
+        }
+    }
+}
+
+// ##### 컨트랙트 함수 정의
+#[near_bindgen]
+impl GuestBook {
+    pub fn get_posted_messages(&self) -> Vec<PostedMessage> {
+        return self.posted_messages.to_vec();
+    }
+
+    pub fn add_posted_message(&mut self, message: String) {
+        let sender = env::predecessor_account_id();
+        let posted_message = PostedMessage { message, sender };
+        self.posted_messages.push(&posted_message);
+    }
+
+    pub fn get_posted_messages_length(&self) -> u64 {
+        return self.posted_messages.len();
+    }
 }
 
 // // ###### 디폴트 코드
@@ -55,31 +99,25 @@ pub struct postedMessage {
 //     }
 // }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
+// /*
+//  * The rest of this file holds the inline tests for the code above
+//  * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+//  */
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
-    }
+//     #[test]
+//     fn get_default_greeting() {
+//         let contract = Contract::default();
+//         // this test did not call set_greeting so should return the default "Hello" greeting
+//         assert_eq!(contract.get_greeting(), "Hello".to_string());
+//     }
 
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
-    }
-}
+//     #[test]
+//     fn set_then_get_greeting() {
+//         let mut contract = Contract::default();
+//         contract.set_greeting("howdy".to_string());
+//         assert_eq!(contract.get_greeting(), "howdy".to_string());
+//     }
+// }
