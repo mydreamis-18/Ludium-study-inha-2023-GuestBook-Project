d8bd412b54a0067ea87538545e17e90e45223474
mydreamis-18 <hitoryok@naver.com>
2023-08-12 11:16:25 +0900

    [Feat] 비로그인 시의 라우터 코드 작성

--- frontend/index.js
+++ frontend/index.js
@@ -40,7 +40,7 @@ window.onload = async () => {
   ReactDOM.render(
     <BrowserRouter>
       <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />
-      <Router />
+      <Router isSignedIn={isSignedIn} />
     </BrowserRouter>,
     document.getElementById('root'),
   );
--- frontend/router.js
+++ frontend/router.js
@@ -8,7 +8,11 @@ import GuestBook_jiwon from './guestBook-components/guestBook_jiwon';
 // ##### 1. 여기에 복사한 컴포넌트 가져오고
 // ##### 2. 아래에 경로 및 렌더링할 컴포넌트로 추가하세요.
 
-const Router = () => {
+const Router = ({ isSignedIn }) => {
+  //
+  // ##### 로그인이 안 되어 있을 경우 안 보이기
+  if (isSignedIn === false) return useRoutes([{ path: "/", element: <></> }]);
+
   return useRoutes([
     {
       path: "/",
